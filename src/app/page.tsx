3c554508c22576bb6e03c5f5949213983ac3ef6b
Heberty Silva <heberty.silva@unimednatal.com.br>
2023-10-04 03:24:55 -0300

    Título da página e imagem do pokemonn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,63 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import { Col, Container, Row } from "react-bootstrap"
-import Pokedex from "./components/Pokedex"
-import CardPokemon from "./components/Pokedex"
+import Pokedex from "./components/CardPokemon"
+import CardPokemon from "./components/CardPokemon"
 
 const page: React.FC = () => {
   const [pokemonData, setPokemonData] = useState([])
 
-  const getPokemons = () => {
-    axios.get("https://pokeapi.co/api/v2/pokemon")
-     .then((response) => {
-       setPokemonData(response.data.results)
-     })
+  const getPokemons = async () => {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon")
+    const results = response.data.results
+
+    const pokemonWithImages:any = await Promise.all(
+      results.map( async (element:any) => {
+        const moreInfo = await getMoreInfo(element.name)
+        const specie = await getSpecie(moreInfo.species.url)
+        return ({
+          name: element.name,
+          urlImage: moreInfo.sprites.front_default,
+          especies: specie.egg_groups
+        })
+    }))
+    
+    setPokemonData(pokemonWithImages)
+  }
+
+  const getMoreInfo = async (pokemon:string) => {
+    const sprites = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((response) => {
+      return response.data
+    })
+
+    return sprites
+  }
+
+  const getSpecie = async (url:string) => {
+    const result = await axios.get(url)
+    .then((response) => {
+      return response.data
+    })
+
+    return result
   }
 
   useEffect(() => {
     getPokemons()
   }, [])
+  
+  useEffect(() => {
+    console.log(pokemonData)
+  })
 
   return(
     <Container>
       <Row>
         {pokemonData.map((pokemon:any, key) => (
-          <Col md={6} lg={4} key={key}>
+          <Col md={6} lg={3} xl={2} key={key}>
               <CardPokemon
+                urlImage={pokemon.urlImage}
                 name={pokemon.name}
               ></CardPokemon>
           </Col>
